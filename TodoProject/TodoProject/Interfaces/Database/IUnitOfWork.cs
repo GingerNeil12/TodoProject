87f4290bac545a35022e59cd05d70170c1b7f4d0
Neil Earlam <neil.earlam@clearskylogic.com>
2020-04-15 00:53:56 +0100

    Added IUnitOfWork and IRepository

---
+++ TodoProject/TodoProject/Interfaces/Database/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TodoProject.Interfaces.Database
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangeAsync();
+    }
+}
---
+++ TodoProject/TodoProject/Interfaces/Database/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace TodoProject.Interfaces.Database.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T model);
+        Task UpdateAsync(T model);
+    }
+}
